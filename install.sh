c56edcaf3ed8b94905164c65c8d1b690d7c4f1f7
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-12-20 12:04:41 -0300

    Global keyboard shortcuts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # dependencies
-sudo pacman -S base-devel curl lua fzf exa tmux fish
+sudo pacman -S base-devel curl lua fzf exa tmux fish fish rofi
 
 # vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
