a83c1a15a28fd7ae3acff408ac22d5da5950cf34
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-12-22 13:37:02 -0300

    fix fish plugins file

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # dependencies
-sudo dnf groupinstall "Development Tools" "Development Libraries"
-sudo dnf install lua5.3 fzf exa tmux fish kitty rofi
+sudo apt install build-essential curl lua5.3 fzf exa tmux
+
+# fish
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update && sudo apt install fish
 
 # vim plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
--- symbol.sh
+++ symbol.sh
@@ -1,7 +1,7 @@
 #!/bin/fish
 
 ln -sf ~/projects/dotfiles/fish/config.fish ~/.config/fish
-ln -sf ~/projects/dotfiles/fish/fish_plugins ~/.config/fish
+cp -f ~/projects/dotfiles/fish/fish_plugins ~/.config/fish
 ln -sf ~/projects/dotfiles/nvim ~/.config
 ln -sf ~/projects/dotfiles/kitty ~/.config
 ln -sf ~/projects/dotfiles/extras/.ideavimrc ~
