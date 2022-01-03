37e2bf58715d033f0f6d3aa2e4a942b29e08cdaa
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-01-03 17:52:29 -0300

    kitty removed

--- symbol.sh
+++ symbol.sh
@@ -4,7 +4,6 @@ ln -sf ~/projects/dotfiles/fish/config.fish ~/.config/fish
 cp -f ~/projects/dotfiles/fish/fish_plugins ~/.config/fish
 ln -sf ~/projects/dotfiles/fish/tide.fish ~/.config/fish/conf.d
 ln -sf ~/projects/dotfiles/nvim ~/.config
-ln -sf ~/projects/dotfiles/kitty ~/.config
 ln -sf ~/projects/dotfiles/extras/.ideavimrc ~
 ln -sf ~/projects/dotfiles/extras/.tmux.conf ~
 ln -sf ~/projects/dotfiles/extras/.gitconfig ~
