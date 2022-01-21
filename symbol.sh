3af35db3a1ca973a13b8319568a390a169f0d1ca
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-01-21 12:10:52 -0300

    Removed neovim

--- symbol.sh
+++ symbol.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp -f ~/projects/dotfiles/zsh/.zshrc ~
-ln -sf ~/projects/dotfiles/nvim ~/.config
 ln -sf ~/projects/dotfiles/extras/.ideavimrc ~
 ln -sf ~/projects/dotfiles/extras/.tmux.conf ~
 ln -sf ~/projects/dotfiles/extras/.gitconfig ~
