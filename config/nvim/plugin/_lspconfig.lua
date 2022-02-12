896e84074dec070feff53ea43f0f7dbf30f9dd54
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-02-12 14:33:13 -0300

    Neovim: Add omnisharp lsp. Tmux: setup mouse support

--- config/nvim/plugin/_lspconfig.lua
+++ config/nvim/plugin/_lspconfig.lua
@@ -34,3 +34,14 @@ require'lspconfig'.jsonls.setup{
 require'lspconfig'.ccls.setup{
     capabilities = capabilities
 }
+
+-- CSharp
+local pid = vim.fn.getpid()
+-- On linux/darwin if using a release build, otherwise under scripts/OmniSharp(.Core)(.cmd)
+local omnisharp_bin = "/home/taauhsz/.local/share/omnisharp/OmniSharp"
+-- on Windows
+-- local omnisharp_bin = "/path/to/omnisharp/OmniSharp.exe"
+require'lspconfig'.omnisharp.setup{
+    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) };
+    ...
+}
