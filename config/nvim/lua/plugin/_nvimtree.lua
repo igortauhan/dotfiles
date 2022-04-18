09ac93bc38b29bdad3a8d4967d2971a0f19d8139
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-04-18 11:25:34 -0300

    Nvim Tree configs

--- config/nvim/lua/plugin/_nvimtree.lua
+++ config/nvim/lua/plugin/_nvimtree.lua
@@ -1,1 +1,11 @@
-require'nvim-tree'.setup()
+require'nvim-tree'.setup {
+  view = {
+    number = true,
+    relativenumber = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true
+    }
+  }
+}
